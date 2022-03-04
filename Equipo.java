656cafec1c03b3152299391dbaee18e5d1422826
Juan <jgodoy@gmail.com>
2022-03-04 14:48:03 -0600

    Commit 3

--- Equipo.java
+++ Equipo.java
@@ -10,5 +10,66 @@ package Laboratorio7;
  * @author jcgof
  */
 public class Equipo {
-    
+
+    private String nombre;
+    private int partidosGanados, partidosEmpatados, partidosPerdidos;
+    private int golesFavor, golesContra;
+
+    public Equipo(String nombre, int partidosGanados, int partidosEmpatados, int partidosPerdidos, int golesFavor, int golesContra) {
+        this.nombre = nombre;
+        this.partidosGanados = partidosGanados;
+        this.partidosEmpatados = partidosEmpatados;
+        this.partidosPerdidos = partidosPerdidos;
+        this.golesFavor = golesFavor;
+        this.golesContra = golesContra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPartidosGanados() {
+        return partidosGanados;
+    }
+
+    public void setPartidosGanados(int partidosGanados) {
+        this.partidosGanados = partidosGanados;
+    }
+
+    public int getPartidosEmpatados() {
+        return partidosEmpatados;
+    }
+
+    public void setPartidosEmpatados(int partidosEmpatados) {
+        this.partidosEmpatados = partidosEmpatados;
+    }
+
+    public int getPartidosPerdidos() {
+        return partidosPerdidos;
+    }
+
+    public void setPartidosPerdidos(int partidosPerdidos) {
+        this.partidosPerdidos = partidosPerdidos;
+    }
+
+    public int getGolesFavor() {
+        return golesFavor;
+    }
+
+    public void setGolesFavor(int golesFavor) {
+        this.golesFavor = golesFavor;
+    }
+
+    public int getGolesContra() {
+        return golesContra;
+    }
+
+    public void setGolesContra(int golesContra) {
+        this.golesContra = golesContra;
+    }
+
 }
